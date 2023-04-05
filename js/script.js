014bc9c5c05d0d00951459988d3217b6ca31812f
jlizardi1 <122846079+jlizardi1@users.noreply.github.com>
2023-04-04 22:21:41 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-function init(){
-  var button = document.getElementById('entrybutton');
+  function init(){
+    var button = document.getElementById('entrybutton');
 
-  function showMeText(){
-    var textbox = document.getElementById('entrybutton');
-    document.getElementById('textoutput').innerHTML = textbox.value;
-    alert("Jeremiah  Lizardi: " + textbox.value);
-    }
+    function showMeText(){
+      var textbox = document.getElementById('entrybutton');
+      document.getElementById('textoutput').innerHTML = textbox.value;
+      alert("Jeremiah  Lizardi: " + textbox.value);
+      }
 
-    button.addEventListener('click', showMeText);
-    }
+      button.addEventListener('click', showMeText);
+      }
 
-window.addEventListener('load', init);
+  window.addEventListener('load', init);
